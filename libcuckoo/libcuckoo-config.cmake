32af54aaa0fa7193a3413ac977a1d17628c59fa8
Kumar Shaurya Shankar <icoderaven@gmail.com>
2020-06-10 23:30:24 -0400

    Minor consistency enhancements. Now applies namespace to installed export targets.

--- libcuckoo/libcuckoo-config.cmake
+++ libcuckoo/libcuckoo-config.cmake
@@ -2,9 +2,12 @@
 # libcuckoo-config.cmake
 #
 
-include (CMakeFindDependencyMacro)
+include(CMakeFindDependencyMacro)
 
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_dependency(Threads)
 
-include ("${CMAKE_CURRENT_LIST_DIR}/libcuckoo-targets.cmake")
+# If this file hasn't been called before
+if(NOT TARGET libcuckoo::libcuckoo)
+  include("${CMAKE_CURRENT_LIST_DIR}/libcuckoo-targets.cmake")
+endif()
